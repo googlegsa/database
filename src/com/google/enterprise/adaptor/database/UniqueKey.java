8a39a0e4597c63b4dd1f72c40d70230262e633cc
PJ <git-pjo.google.com>
2015-02-12 19:38:05 -0800

    one import re-order

--- src/com/google/enterprise/adaptor/database/UniqueKey.java
+++ src/com/google/enterprise/adaptor/database/UniqueKey.java
@@ -21,8 +21,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
