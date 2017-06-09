googlegsa/database

commit d080ea5c7e2c3f84881b03c1f6be0ce86b77555d

Use try-with-resources and canonical getters in TupleReader

src/com/google/enterprise/adaptor/database/TupleReader.java
@@ -25,7 +25,11 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.io.StringReader;
 import java.io.Writer;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.NClob;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -36,6 +40,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.xml.bind.DatatypeConverter;
@@ -138,7 +143,6 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
     ResultSetMetaData rsmd = resultSet.getMetaData();
     int colCount = rsmd.getColumnCount();
 
-    Writer outWriter = new BufferedWriter(new ContentHandlerWriter(handler));
     for (int col = 1; col <= colCount; col++) {
       atts.clear();
       String columnName = rsmd.getColumnLabel(col);
@@ -148,52 +152,118 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
         atts.addAttribute("", "SQLType", "SQLType", "NMTOKEN", sqlTypeName);
       }
       log.fine("sqlTypeName: " + sqlTypeName);
-      if (resultSet.getObject(col) == null) {
-        atts.addAttribute("", "ISNULL", "ISNULL", "NMTOKEN", "true");
-        handler.startElement("", columnName, columnName, atts);
-        // TODO: Re-examine this need for flush.
-        outWriter.flush();
-        handler.endElement("", columnName, columnName);
-        continue;
-      }
       switch (sqlType) {
         case Types.DATE:
           Date dateValue = resultSet.getDate(col);
-          handler.startElement("", columnName, columnName, atts);
-          outWriter.write(DATEFMT.format(dateValue));
+          if (dateValue != null) {
+            try (Writer outWriter =
+                new BufferedWriter(new ContentHandlerWriter(handler))) {
+              handler.startElement("", columnName, columnName, atts);
+              outWriter.write(DATEFMT.format(dateValue));
+            }
+          }
           break;
         case Types.TIMESTAMP:
           // TODO: probably need to change to calling getTimestamp with Calendar
           // parameter.
           Timestamp tsValue = resultSet.getTimestamp(col);
-          handler.startElement("", columnName, columnName, atts);
-          String timezone = TIMEZONEFMT.format(tsValue);
-          // java timezone not ISO compliant
-          timezone =
-              timezone.substring(0, timezone.length() - 2) + ":"
-                  + timezone.substring(timezone.length() - 2);
-          outWriter.write(DATEFMT.format(tsValue) + "T"
-              + TIMEFMT.format(tsValue) + timezone);
+          if (tsValue != null) {
+            try (Writer outWriter =
+                new BufferedWriter(new ContentHandlerWriter(handler))) {
+              handler.startElement("", columnName, columnName, atts);
+              String timezone = TIMEZONEFMT.format(tsValue);
+              // java timezone not ISO compliant
+              timezone =
+                  timezone.substring(0, timezone.length() - 2) + ":"
+                      + timezone.substring(timezone.length() - 2);
+              outWriter.write(DATEFMT.format(tsValue) + "T"
+                  + TIMEFMT.format(tsValue) + timezone);
+            }
+          }
           break;
         case Types.TIME:
           Time timeValue = resultSet.getTime(col);
-          handler.startElement("", columnName, columnName, atts);
-          timezone = TIMEZONEFMT.format(timeValue);
-          // java timezone not ISO compliant
-          timezone =
-              timezone.substring(0, timezone.length() - 2) + ":"
-                  + timezone.substring(timezone.length() - 2);
-          outWriter.write(TIMEFMT.format(timeValue) + timezone);
+          if (timeValue != null) {
+            try (Writer outWriter =
+                new BufferedWriter(new ContentHandlerWriter(handler))) {
+              handler.startElement("", columnName, columnName, atts);
+              String timezone = TIMEZONEFMT.format(timeValue);
+              // java timezone not ISO compliant
+              timezone =
+                  timezone.substring(0, timezone.length() - 2) + ":"
+                      + timezone.substring(timezone.length() - 2);
+              outWriter.write(TIMEFMT.format(timeValue) + timezone);
+            }
+          }
           break;
         case Types.BLOB:
+          Blob blob = resultSet.getBlob(col);
+          if (blob != null) {
+            try (InputStream lob = resultSet.getBinaryStream(col);
+                Writer writer =
+                    new BufferedWriter(new ContentHandlerWriter(handler))) {
+              // so encode using Base64
+              atts.addAttribute("", "encoding", "encoding", "NMTOKEN",
+                  "base64binary");
+              handler.startElement("", columnName, columnName, atts);
+              encode(lob, writer);
+            } finally {
+              try {
+                blob.free();
+              } catch (Exception e) {
+                log.log(Level.FINEST, "Error closing BLOB", e);
+              }
+            }
+          }
+          break;
+        case Types.CLOB:
+          Clob clob = resultSet.getClob(col);
+          if (clob != null) {
+            try (Reader reader = clob.getCharacterStream();
+                Writer writer =
+                    new BufferedWriter(new ContentHandlerWriter(handler))) {
+              handler.startElement("", columnName, columnName, atts);
+              copyValidXMLCharacters(reader, writer);
+            } finally {
+              try {
+                clob.free();
+              } catch (Exception e) {
+                log.log(Level.FINEST, "Error closing CLOB", e);
+              }
+            }
+          }
+          break;
+        case Types.NCLOB:
+          NClob nclob = resultSet.getNClob(col);
+          if (nclob != null) {
+            try (Reader reader = resultSet.getNCharacterStream(col);
+                Writer writer =
+                    new BufferedWriter(new ContentHandlerWriter(handler))) {
+              handler.startElement("", columnName, columnName, atts);
+              copyValidXMLCharacters(reader, writer);
+            } finally {
+              try {
+                nclob.free();
+              } catch (Exception e) {
+                log.log(Level.FINEST, "Error closing NCLOB", e);
+              }
+            }
+          }
+          break;
+        case Types.BINARY:
         case Types.VARBINARY:
         case Types.LONGVARBINARY:
-          InputStream lob = resultSet.getBinaryStream(col);
-          // so encode using Base64
-          atts.addAttribute("", "encoding", "encoding", "NMTOKEN",
-              "base64binary");
-          handler.startElement("", columnName, columnName, atts);
-          encode(lob, outWriter);
+          try (InputStream lob = resultSet.getBinaryStream(col);
+              Writer outWriter =
+                  new BufferedWriter(new ContentHandlerWriter(handler))) {
+            if (lob != null) {
+              // so encode using Base64
+              atts.addAttribute("", "encoding", "encoding", "NMTOKEN",
+                  "base64binary");
+              handler.startElement("", columnName, columnName, atts);
+              encode(lob, outWriter);
+            }
+          }
           break;
         case Types.SMALLINT:
         case Types.TINYINT:
@@ -202,33 +272,58 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
         case Types.NUMERIC:
         case Types.DOUBLE:
         case Types.INTEGER:
+          String string = resultSet.getString(col);
+          if (string != null) {
+            try (Writer outWriter =
+                new BufferedWriter(new ContentHandlerWriter(handler))) {
+              handler.startElement("", columnName, columnName, atts);
+              outWriter.write(string);
+            }
+          }
+          break;
         case Types.CHAR:
         case Types.VARCHAR:
-          String string = resultSet.getString(col);
-          handler.startElement("", columnName, columnName, atts);
-          outWriter.write(string);
-          break;
-        case Types.CLOB:
-        case Types.LONGVARCHAR:
-          Reader reader = resultSet.getCharacterStream(col);
-          handler.startElement("", columnName, columnName, atts);
-          copyValidXMLCharacters(reader, outWriter);
-          reader.close();
-          break;
         case Types.NCHAR:
-        case Types.NCLOB:
         case Types.NVARCHAR:
+          string = resultSet.getString(col);
+          if (string != null) {
+            try (Writer outWriter =
+                new BufferedWriter(new ContentHandlerWriter(handler))) {
+              handler.startElement("", columnName, columnName, atts);
+              copyValidXMLCharacters(new StringReader(string), outWriter);
+            }
+          }
+          break;
+        case Types.LONGVARCHAR:
+          try (Reader reader = resultSet.getCharacterStream(col);
+              Writer outWriter =
+                  new BufferedWriter(new ContentHandlerWriter(handler))) {
+            if (reader != null) {
+              handler.startElement("", columnName, columnName, atts);
+              copyValidXMLCharacters(reader, outWriter);
+            }
+          }
+          break;
         case Types.LONGNVARCHAR:
-          Reader nReader = resultSet.getNCharacterStream(col);
-          handler.startElement("", columnName, columnName, atts);
-          copyValidXMLCharacters(nReader, outWriter);
-          nReader.close();
+          try (Reader nReader = resultSet.getNCharacterStream(col);
+              Writer outWriter =
+                  new BufferedWriter(new ContentHandlerWriter(handler))) {
+            if (nReader != null) {
+              handler.startElement("", columnName, columnName, atts);
+              copyValidXMLCharacters(nReader, outWriter);
+            }
+          }
           break;
         case Types.BIT:
         case Types.BOOLEAN:
-          Boolean value = resultSet.getBoolean(col);
-          handler.startElement("", columnName, columnName, atts);
-          outWriter.write(String.valueOf(value));
+          Object value = resultSet.getObject(col);
+          if (value != null) {
+            try (Writer outWriter =
+                new BufferedWriter(new ContentHandlerWriter(handler))) {
+              handler.startElement("", columnName, columnName, atts);
+              outWriter.write(String.valueOf(value));
+            }
+          }
           break;
         case Types.OTHER:
         default:
@@ -236,14 +331,19 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
           if (null == string) {
             string = "" + resultSet.getObject(col);
           }
-          handler.startElement("", columnName, columnName, atts);
-          outWriter.write(string);
+          try (Writer writer =
+              new BufferedWriter(new ContentHandlerWriter(handler))) {
+            handler.startElement("", columnName, columnName, atts);
+            writer.write(string);
+          }
           break;
       }
-      outWriter.flush();
+      if (resultSet.wasNull()) {
+        atts.addAttribute("", "ISNULL", "ISNULL", "NMTOKEN", "true");
+        handler.startElement("", columnName, columnName, atts);
+      }
       handler.endElement("", columnName, columnName);
     }
-    outWriter.close();
     if (recordElement != null) {
       handler.endElement(ns, recordElement, recordElement);
     }

commit f6084f69e228a65cc60b638c8b4479b12321448f

Improve code coverage for TupleReader.
Improves coverage of TupleReader from 65% to 75%.

Production changes:

* Replace the getColumnTypeName switch implementation with a map from
  java.sql.Types field values to their names. This avoids bloating the
  tests with similar types (e.g., REAL and FLOAT), and avoids having
  missing entries in the switch statement (for example, ROWID and
  SQLXML were missing).

* Remove unused constructor overload, and hard-code element names with
  the values from the other overload.

* Fix a bug introduced in commit d080ea5 with null values of SQL type
  OTHER. Nulls were turned into "null" and handler.startElement was
  called, but the new ResultSet.wasNull check was also true, and
  handler.startElement was called again. Only call getObject, because
  getString might throw a SQLException for OTHER and other unsupported
  types.

src/com/google/enterprise/adaptor/database/TupleReader.java
@@ -26,6 +26,8 @@ import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.Writer;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.sql.Blob;
 import java.sql.Clob;
 import java.sql.NClob;
@@ -39,6 +41,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -80,26 +83,35 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
   private static final DateFormat TIMEFMT = new SimpleDateFormat("hh:mm:ss");
   private static final DateFormat TIMEZONEFMT = new SimpleDateFormat("Z");
 
-  private final ResultSet resultSet;
-  private final String ns; // namespace
-  private final String rootElement;
-  private final String tableElement;
-  private final String recordElement;
+  /** Map from SQL types to SQL type names. */
+  private static final HashMap<Integer, String> sqlTypeNames = new HashMap<>();
 
-  public TupleReader(ResultSet resultSet) {
-    this(resultSet, "", "database", "table", "table_rec");
+  static {
+    for (Field field : Types.class.getFields()) {
+      if (field.getType() == int.class
+          && (field.getModifiers() & Modifier.STATIC) == Modifier.STATIC) {
+        try {
+          sqlTypeNames.put(field.getInt(null), field.getName());
+        } catch (IllegalAccessException | IllegalArgumentException e) {
+          // These should not be possible with the checks above.
+          throw new AssertionError(e);
+        }
+      }
+    }
   }
 
-  public TupleReader(ResultSet resultSet, String ns, String rootElement,
-      String tableElement, String recordElement) {
+  private static final String NS = ""; // namespace
+  private static final String ROOT_ELEMENT = "database";
+  private static final String TABLE_ELEMENT = "table";
+  private static final String RECORD_ELEMENT = "table_rec";
+
+  private final ResultSet resultSet;
+
+  public TupleReader(ResultSet resultSet) {
     if (resultSet == null) {
       throw new NullPointerException();
     }
     this.resultSet = resultSet;
-    this.ns = ns;
-    this.rootElement = rootElement;
-    this.tableElement = tableElement;
-    this.recordElement = recordElement;
   }
 
   /**
@@ -114,19 +126,11 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
       ContentHandler handler = super.getContentHandler();
       AttributesImpl atts = new AttributesImpl();
       handler.startDocument();
-      if (rootElement != null) {
-        handler.startElement(ns, rootElement, rootElement, atts);
-      }
-      if (tableElement != null) {
-        handler.startElement(ns, tableElement, tableElement, atts);
-      }
+      handler.startElement(NS, ROOT_ELEMENT, ROOT_ELEMENT, atts);
+      handler.startElement(NS, TABLE_ELEMENT, TABLE_ELEMENT, atts);
       emitRow(resultSet, handler);
-      if (tableElement != null) {
-        handler.endElement(ns, tableElement, tableElement);
-      }
-      if (rootElement != null) {
-        handler.endElement(ns, rootElement, rootElement);
-      }
+      handler.endElement(NS, TABLE_ELEMENT, TABLE_ELEMENT);
+      handler.endElement(NS, ROOT_ELEMENT, ROOT_ELEMENT);
       handler.endDocument();
     } catch (SQLException ex) {
       throw new SAXException(ex);
@@ -136,9 +140,7 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
   protected void emitRow(ResultSet resultSet, ContentHandler handler)
       throws SQLException, SAXException, IOException {
     AttributesImpl atts = new AttributesImpl();
-    if (recordElement != null) {
-      handler.startElement(ns, recordElement, recordElement, atts);
-    }
+    handler.startElement(NS, RECORD_ELEMENT, RECORD_ELEMENT, atts);
     ResultSetMetaData rsmd = resultSet.getMetaData();
     int colCount = rsmd.getColumnCount();
 
@@ -146,10 +148,8 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
       atts.clear();
       String columnName = rsmd.getColumnLabel(col);
       int sqlType = rsmd.getColumnType(col);
-      String sqlTypeName = getColumnTypeName(sqlType);
-      if (sqlTypeName != null) {
-        atts.addAttribute("", "SQLType", "SQLType", "NMTOKEN", sqlTypeName);
-      }
+      String sqlTypeName = getColumnTypeName(sqlType, rsmd, col);
+      atts.addAttribute("", "SQLType", "SQLType", "NMTOKEN", sqlTypeName);
       log.fine("sqlTypeName: " + sqlTypeName);
       switch (sqlType) {
         case Types.DATE:
@@ -308,13 +308,12 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
           break;
         case Types.OTHER:
         default:
-          string = resultSet.getString(col);
-          if (null == string) {
-            string = "" + resultSet.getObject(col);
-          }
-          try (Writer writer = new ContentHandlerWriter(handler)) {
-            handler.startElement("", columnName, columnName, atts);
-            writer.write(string);
+          value = resultSet.getObject(col);
+          if (value != null) {
+            try (Writer writer = new ContentHandlerWriter(handler)) {
+              handler.startElement("", columnName, columnName, atts);
+              writer.write(String.valueOf(value));
+            }
           }
           break;
       }
@@ -324,93 +323,30 @@ class TupleReader extends XMLFilterImpl implements XMLReader {
       }
       handler.endElement("", columnName, columnName);
     }
-    if (recordElement != null) {
-      handler.endElement(ns, recordElement, recordElement);
-    }
+    handler.endElement(NS, RECORD_ELEMENT, RECORD_ELEMENT);
   }
 
   /**
    * Return a SQL type name for the column type.
    * Use this instead of {@link ResultSetMetaData.getColumnTypeName()} 
-   * because drivers return different names. MySQL returns very stupid
-   * names (TEXT => "BLOB").
+   * because drivers return different names.
    *
    * @param sqlType a type from java.sql.Types
-   * @return a string name, null if sqlType is unknown
+   * @return a name for the type from the first non-null value found
+   *     looking in java.sql.Types, calling ResultSetMetaData.getColumnTypeName,
+   *      and falling back to a string of the raw integer type itself
    */
-  private static String getColumnTypeName(int sqlType) {
-    switch (sqlType) {
-      case Types.BIT:
-        return "BIT";
-      case Types.TINYINT:
-        return "TINYINT";
-      case Types.SMALLINT:
-        return "SMALLINT";
-      case Types.INTEGER:
-        return "INTEGER";
-      case Types.BIGINT:
-        return "BIGINT";
-      case Types.FLOAT:
-        return "FLOAT";
-      case Types.REAL:
-        return "REAL";
-      case Types.DOUBLE:
-        return "DOUBLE";
-      case Types.NUMERIC:
-        return "NUMERIC";
-      case Types.DECIMAL:
-        return "DECIMAL";
-      case Types.CHAR:
-        return "CHAR";
-      case Types.VARCHAR:
-        return "VARCHAR";
-      case Types.LONGVARCHAR:
-        return "LONGVARCHAR";
-      case Types.DATE:
-        return "DATE";
-      case Types.TIME:
-        return "TIME";
-      case Types.TIMESTAMP:
-        return "TIMESTAMP";
-      case Types.BINARY:
-        return "BINARY";
-      case Types.VARBINARY:
-        return "VARBINARY";
-      case Types.LONGVARBINARY:
-        return "LONGVARBINARY";
-      case Types.NULL:
-        return "NULL";
-      case Types.OTHER:
-        return "OTHER";
-      case Types.JAVA_OBJECT:
-        return "JAVA_OBJECT";
-      case Types.DISTINCT:
-        return "DISTINCT";
-      case Types.STRUCT:
-        return "STRUCT";
-      case Types.ARRAY:
-        return "ARRAY";
-      case Types.BLOB:
-        return "BLOB";
-      case Types.CLOB:
-        return "CLOB";
-      case Types.REF:
-        return "REF";
-      case Types.DATALINK:
-        return "DATALINK";
-      case Types.BOOLEAN:
-        return "BOOLEAN";
-      case Types.NCHAR:
-        return "NCHAR";
-      case Types.NVARCHAR:
-        return "NVARCHAR";
-      case Types.NCLOB:
-        return "NCLOB";
-      case Types.LONGNVARCHAR:
-        return "LONGNVARCHAR";
-      default:
-        return String.valueOf(sqlType);
-    }
+  private static String getColumnTypeName(int sqlType, ResultSetMetaData rsmd,
+      int columnIndex) throws SQLException {
+     String sqlTypeName = sqlTypeNames.get(sqlType);
+     if (sqlTypeName == null) {
+       // Try the database's name for non-standard types.
+       sqlTypeName = rsmd.getColumnTypeName(columnIndex);
+       if (sqlTypeName == null || sqlTypeName.isEmpty()) {
+         sqlTypeName = String.valueOf(sqlType);
+       }
+     }
+     return sqlTypeName;
   }
 
   /**

test/com/google/enterprise/adaptor/database/TupleReaderTest.java
@@ -14,10 +14,12 @@
 
 package com.google.enterprise.adaptor.database;
 
+import static com.google.enterprise.adaptor.database.JdbcFixture.executeQuery;
 import static com.google.enterprise.adaptor.database.JdbcFixture.executeQueryAndNext;
 import static com.google.enterprise.adaptor.database.JdbcFixture.executeUpdate;
 import static com.google.enterprise.adaptor.database.JdbcFixture.getConnection;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.CoreMatchers.isA;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
@@ -25,8 +27,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
 import java.io.StringWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -76,6 +81,22 @@ public class TupleReaderTest {
     Result des = new StreamResult(writer);
     trans.transform(source, des);
     return writer.toString();
+  }
+
+  @Test
+  public void testConstructor_nullResultSet() {
+    thrown.expect(NullPointerException.class);
+    new TupleReader(null);
+  }
+
+  @Test
+  public void testParse_emptyResultSet() throws Exception {
+    executeUpdate("create table data(colname varchar)");
+    ResultSet rs = executeQuery("select * from data");
+    thrown.expect(TransformerException.class);
+    thrown.expectCause(isA(SAXException.class));
+    thrown.expectMessage("No data is available");
+    generateXml(rs);
   }
 
   /**
@@ -492,6 +513,53 @@ public class TupleReaderTest {
         + "<table>"
         + "<table_rec>"
         + "<COLNAME SQLType=\"CLOB\" ISNULL=\"true\"/>"
+        + "</table_rec>"
+        + "</table>"
+        + "</database>";
+    ResultSet rs = executeQueryAndNext("select * from data");
+    String result = generateXml(rs);
+    assertEquals(golden, result);
+  }
+
+  @Test
+  public void testOther() throws Exception {
+    // H2's OTHER type requires a serialized Java object.
+    String serializable = "hello world";
+    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    try (ObjectOutputStream out = new ObjectOutputStream(buffer)) {
+      out.writeObject(serializable);
+    }
+
+    executeUpdate("create table data(colname other)");
+    String sql = "insert into data(colname) values (?)";
+    try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+      ps.setObject(1, buffer.toByteArray());
+      assertEquals(1, ps.executeUpdate());
+    }
+    final String golden = ""
+        + "<database>"
+        + "<table>"
+        + "<table_rec>"
+        + "<COLNAME SQLType=\"OTHER\">"
+        + serializable
+        + "</COLNAME>"
+        + "</table_rec>"
+        + "</table>"
+        + "</database>";
+    ResultSet rs = executeQueryAndNext("select * from data");
+    String result = generateXml(rs);
+    assertEquals(golden, result);
+  }
+
+  @Test
+  public void testOther_null() throws Exception {
+    executeUpdate("create table data(other other)");
+    executeUpdate("insert into data(other) values(null)");
+    final String golden = ""
+        + "<database>"
+        + "<table>"
+        + "<table_rec>"
+        + "<OTHER SQLType=\"OTHER\" ISNULL=\"true\"/>"
         + "</table_rec>"
         + "</table>"
         + "</database>";